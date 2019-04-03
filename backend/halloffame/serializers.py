44bd6e3cd0a136930f57baca0e700f33e95e3cc6
Smoczewski <krzysztof.szewczyk124@gmail.com>
2019-04-03 16:32:32 +0200

    REMOVED:  - Fraction from serializer

--- backend/halloffame/serializers.py
+++ backend/halloffame/serializers.py
@@ -3,11 +3,10 @@ from rest_framework import serializers
 from .models import Hero
 
 
-class HeroSerializer(ModelSerializer):
+class HeroesRankingSerializer(ModelSerializer):
     user_name = serializers.CharField(source='user', read_only=True)
 
     class Meta:
         model = Hero
-        fields = ('user_name', 'race', 'fraction', 'guild', 'level',
+        fields = ('user_name', 'race', 'guild', 'level',
                   'death_date', 'battles_won', 'battles_lost', 'death_date')
-
