cbece21d967451fe5e18c79342862b1d62780575
Smoczewski <krzysztof.szewczyk124@gmail.com>
2019-05-08 15:15:25 +0200

    Added rounds as nested endpoint, fixed permissions bug  - Added read_only_fields to Battle_serializer

--- backend/battles/permissions.py
+++ backend/battles/permissions.py
@@ -2,8 +2,8 @@ from rest_framework import permissions
 
 
 class IsStaffOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-
         return request.user.is_staff
+
--- backend/battles/urls.py
+++ backend/battles/urls.py
@@ -1,15 +1,19 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework_nested import routers
 from rest_framework_swagger.views import get_swagger_view
 
-from . import views
+from battles import views
 
 schema_view = get_swagger_view(title='Sakaar')
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register('battles', views.BattleViewSet)
+
+battles_router = routers.NestedSimpleRouter(router, 'battles', lookup='battle')
+battles_router.register('rounds', views.RoundViewSet)
 
 urlpatterns = [
     path('', schema_view),
     path('', include(router.urls)),
+    path('', include(battles_router.urls)),
 ]
