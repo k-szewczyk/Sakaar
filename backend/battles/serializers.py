a5598b2c6d33cec5a84131f7809f30f40bb5e876
Smoczewski <krzysztof.szewczyk124@gmail.com>
2019-05-09 11:47:20 +0200

    Added api tests and validation if one of selected attendees is dead
    Tests covers:
     - If only admin can POST battle
     - If two attendees can fight only once with each other
     - If dead hero can not attend in new battle

--- backend/battles/serializers.py
+++ backend/battles/serializers.py
@@ -1,7 +1,10 @@
+from django.db.models import Q
 from rest_framework import serializers
-from battles.services import Fight
 from rest_framework.serializers import ValidationError
+
 from battles.models import Battle, Round
+from battles.services import Fight
+from halloffame.models import Hero
 
 
 class RoundSerializer(serializers.ModelSerializer):
@@ -23,6 +26,15 @@ class BattleSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         attendees = attrs['attendees']
+        annotated_attendees = Hero.objects.get_annotations().filter(Q(user=attendees[0]) | Q(user=attendees[1]))
+        for attendee in annotated_attendees:
+            if not attendee.is_alive:
+                raise ValidationError(f'Couldn\'t create fight player {attendee} is dead')
+
+        races = [hero.race for hero in annotated_attendees]
+        if not races[0] in races[1].can_fight_with.all():
+            raise ValidationError(f'Heroes of races {races[0]} and {races[1]}')
+
         if len(Battle.objects.filter(attendees__user=attendees[0]).filter(attendees__user=attendees[1])) > 0:
             raise ValidationError('Battle with those attendees already exists')
         return attrs
--- backend/halloffame/tests.py
+++ backend/halloffame/tests.py
@@ -1,8 +1,8 @@
 from rest_framework import test, status
 from rest_framework.reverse import reverse
 
-from halloffame.models import Hero
 from halloffame import factories
+from halloffame.models import Hero
 
 
 class HallOfFamePermissionsTestCase(test.APITestCase):
@@ -16,33 +16,33 @@ class HallOfFamePermissionsTestCase(test.APITestCase):
 
     def setUp(self):
         self.user1 = factories.UserFactory()
-        self.user1_url= reverse("hero-detail", args=(self.user1.id,))
+        self.user1_url = reverse("hero-detail", args=(self.user1.id,))
         self.user2 = factories.UserFactory()
         self.user2_url = reverse("hero-detail", args=(self.user2.id,))
         self.client.force_login(self.user1)
 
     def test_create_new_hero(self):
-        response = self.client.post(self.hero_list_url,
-                                    {'user': self.user1.id, 'race': self.race1.id, 'guild': self.guild1.id})
+        self.client.post(self.hero_list_url,
+                         {'user': self.user1.id, 'race': self.race1.id, 'guild': self.guild1.id})
 
         self.assertEqual(Hero.objects.count(), 1)
 
     def test_user_can_not_create_second_hero(self):
         factories.HeroFactory(user=self.user1)
-        response = self.client.post(self.hero_list_url,
-                                    {'user': self.user1.id, 'race': self.race1.id, 'guild': self.guild1.id})
+        self.client.post(self.hero_list_url,
+                         {'user': self.user1.id, 'race': self.race1.id, 'guild': self.guild1.id})
 
         self.assertEqual(Hero.objects.count(), 1)
 
     def test_create_new_hero_assigned_to_someone_else(self):
-        response = self.client.post(self.hero_list_url,
-                                    {'user': self.user2.id, 'race': self.race1.id, 'guild': self.guild1.id})
+        self.client.post(self.hero_list_url,
+                         {'user': self.user2.id, 'race': self.race1.id, 'guild': self.guild1.id})
 
         self.assertEqual(Hero.objects.count(), 0)
 
     def test_delete_someone_elses_hero(self):
         factories.HeroFactory(user=self.user2)
-        response = self.client.delete(self.user2_url)
+        self.client.delete(self.user2_url)
 
         self.assertEqual(Hero.objects.count(), 1)
 
